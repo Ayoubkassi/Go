349d5e7a406f1afe180dee0ad20f372969420dea
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-10-01 16:29:28 +0100

    Go Operators Go Arithmetic Go Relational GO Logical & Assignement

--- first.go
+++ first.go
@@ -4,6 +4,7 @@ import "fmt"
 import "os"
 
 func main(){
+  /////////////////////////////// First Lesson
   var name string
   name = "Ayoub Kassi"
   var slary int = 10
@@ -15,7 +16,7 @@ func main(){
   //inferance method
   number_3 := 30
   // :operator is a short hand for a variable without datatype or var keyword
-  
+
 
 
 
@@ -25,6 +26,58 @@ func main(){
   //V -> VALUE
   fmt.Printf(" %v %v %v %v",name , is_loading , salary, grade)
   //for string we can use %q -> quote
+
+
+  ///////////////////////////////////// Second Lesson
+  ////////////////// OPerator , Relational , Logical and Assignment
+
+
+  var num1 int = 1
+  var num2 int = 2
+  total = num1+num2
+
+  fmt.Printf("%d",num1+num2)   //%d -> digit -> number
+  fmt.Printf("%d",total)
+
+  var sub = num1-num2
+
+  var divide = num1 / num2
+
+  var multi = num1 * num2
+
+  num1++
+  num2--  //we have increment and decrement
+
+  if(num1 == num2){
+    fmt.Println("Equal")
+  }else{
+    fmt.Println("NO doesn\'t match")
+  }
+  //same for relational for > < >= <= !=
+
+  //logical ops
+
+  if(num1 && num2){
+    fmt.Printf("Oook")
+  }fmt.Printf("Niice")
+
+  // also with ||
+
+
+  //Assignment
+
+  num1+=num2
+  num1*=num2
+  num1/=num2
+  num1-=num2
+
+  // modulo operatot %
+
+  
+
+
+
+
 
 }
 
